67c9d5803547a686243be336408c1203a850426f
valentino <126835108+Valen89hh@users.noreply.github.com>
2025-09-04 17:39:50 -0500

    add StudentReport

--- src/main/java/org/example/controller/StudentController.java
+++ src/main/java/org/example/controller/StudentController.java
@@ -1,4 +1,29 @@
 package org.example.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentController {
+
+    private final List<String> students = new ArrayList<>();
+
+    public void addStudent(String name) {
+        students.add(name);
+        System.out.println("Estudiante agregado: " + name);
+    }
+
+    public List<String> getAllStudents() {
+        return students;
+    }
+
+    public String getStudentByIndex(int index) {
+        if (index >= 0 && index < students.size()) {
+            return students.get(index);
+        }
+        return "Estudiante no encontrado";
+    }
+
+    public boolean removeStudent(String name) {
+        return students.remove(name);
+    }
 }
